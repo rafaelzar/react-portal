c01d8a32b7f71e4c990cc535bc7942e953418e6e
rafaelzar <rafaelzaerate@gmail.com>
2021-08-10 17:54:07 +0200

    added selectors, fixed auth actions and added route guarding on home page.

--- src/store/actions/authActions.ts
+++ src/store/actions/authActions.ts
@@ -1,6 +1,7 @@
 import {
   logInUserCognitoFunction,
   logInUserWithNewPasswordCognitoFunction,
+  logOutUserCognitoFunction,
 } from '../../lib/aws/aws-cognito-functions';
 import { AppDispatch } from '../store';
 
@@ -46,6 +47,23 @@ export const logInCognitoUserWithNewPasswordAuthAction = (
       if (user) {
         dispatch({ type: 'LOGIN_SUCCESS', user });
         return user;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+};
+
+export const logOutCognitoUserAuthAction = () => {
+  return async (dispatch: AppDispatch): Promise<boolean> => {
+    try {
+      const res = await logOutUserCognitoFunction();
+      if (res) {
+        dispatch({ type: 'AUTH_LOGOUT_SUCCESS' });
+        return true;
       } else {
         return false;
       }
---
+++ src/store/selectors/selectors.ts
@@ -0,0 +1,13 @@
+export const getUserSelector = (
+  state: any,
+): Record<string, unknown> | Record<string, never> => {
+  const { user = {} } = state;
+  return user || {};
+};
+
+export const getUserJwtTokenSelector = (
+  state: any,
+): string => {
+  const { user: { signInUserSession: { accessToken: { jwtToken = '' } = {} } = {} } = {} } = state;
+  return jwtToken || '';
+};

69fc9207b9a9e57ab9b0db220bbf9c37553646df
rafaelzar <rafaelzaerate@gmail.com>
2021-08-11 14:40:39 +0200

    fixed store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistReducer } from 'redux-persist';
 
-const store = configureStore({
-  reducer: rootReducer,
-});
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
-export type AppDispatch = typeof store.dispatch
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const composeEnhancers = process.env.REACT_APP_ENVIRONMENT === 'development'
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  : compose;
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
+
+export type AppDispatch = typeof store.dispatch;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const useAppDispatch = (): any => useDispatch<AppDispatch>();
 

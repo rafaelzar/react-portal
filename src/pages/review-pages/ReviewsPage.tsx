d4070107988ee51e65db0a21d4519a6150fbc050
rafaelzar <rafaelzaerate@gmail.com>
2021-08-18 11:37:10 +0200

    Added interface and toggle functionality for date-range component

--- src/pages/review-pages/ReviewsPage.tsx
+++ src/pages/review-pages/ReviewsPage.tsx
@@ -12,13 +12,13 @@ import { DateRangePicker } from 'react-date-range';
 import { addDays } from 'date-fns';
 import ReviewCard from '../../components/reviews-page/ReviewCard';
 import { mockupData } from '../../lib/utils/mockupData';
-import { IReviews } from '../../lib/interfaces';
+import { IReviews, IDatePicker } from '../../lib/interfaces';
 import ReviewStats from '../../components/reviews-page/ReviewStats';
-import { createFalse } from 'typescript';
 
 const ReviewsPage: React.FC = () => {
   const [reviews, setReviews] = React.useState<IReviews[]>([]);
-  const [dateState, setDateState] = React.useState<any>([
+  const [toggleDatePicker, setToggleDatePicker] = React.useState(false);
+  const [dateState, setDateState] = React.useState<IDatePicker[]>([
     {
       startDate: new Date(),
       endDate: addDays(new Date(), 7),
@@ -35,27 +35,37 @@ const ReviewsPage: React.FC = () => {
         <h2>Reviews</h2>
         <div className='filters-section d-flex align-items-center mt-3 mb-5'>
           <span>Show reviews from</span>
-          <div className='d-flex align-items-center ml-2'>
-            <DateRangePicker
-              onChange={(item) => setDateState([item.selection])}
-              showSelectionPreview
-              // showMonthAndYearPickers={false}
-              moveRangeOnFirstSelection={false}
-              ranges={dateState}
-              direction='horizontal'
-            />
-            {/* <DropdownButton id='dropdown-reviews-time' title='Last Week'>
-              <Dropdown.Item>Last Week</Dropdown.Item>
-              <Dropdown.Item>Last 4 weeks</Dropdown.Item>
-              <Dropdown.Item>Last 3 Months</Dropdown.Item>
-            </DropdownButton>
-            <input type='date' name='review-date' className='mx-2' /> */}
-            <span className='mr-2'>on</span>
-            <DropdownButton id='dropdown-reviews-site' title='All sites'>
-              <Dropdown.Item>All sites</Dropdown.Item>
-              <Dropdown.Item>Eyerate</Dropdown.Item>
-            </DropdownButton>
+          <div
+            className='date-range-btn-wrapp ml-2'
+            onClick={() => setToggleDatePicker(!toggleDatePicker)}
+          >
+            <div className='date-range-btn'>Last 4 weeks</div>
+            <div className='date-range-btn'>Jun 30-Jul 30</div>
           </div>
+          <div className='d-flex align-items-center ml-2'>
+            <span className='mr-2'>on</span>
+            <div
+              className='date-range-btn with-border d-flex align-items-center'
+              onClick={() => console.log(dateState)}
+            >
+              All Sites
+              <div className='arrow-wrapp'>
+                <i className='arrow down ml-5' />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`date-piker-wrapp ${toggleDatePicker ? 'd-block' : ''}`}
+        >
+          <DateRangePicker
+            onChange={(item) => setDateState([item.selection])}
+            showSelectionPreview
+            // showMonthAndYearPickers={false}
+            moveRangeOnFirstSelection={false}
+            ranges={dateState}
+            direction='horizontal'
+          />
         </div>
         <Row>
           <Col md={4}>

023a1626a3f8496349d5eb1af4d6959f1da98f85
rafaelzar <rafaelzaerate@gmail.com>
2021-08-31 14:48:00 +0200

    Removed comments

--- src/components/home-page/PlaidComponent.tsx
+++ src/components/home-page/PlaidComponent.tsx
@@ -7,12 +7,10 @@ const PlaidComponent: FunctionComponent = () => {
   const [token, setToken] = useState<string | null>(null);
   const userID = '607a1d65e4be5100126b827e';
 
-  // generate a link_token
   React.useEffect(() => {
     async function createLinkToken() {
       const response = await getPlaidLinkToken(userID);
       const { data: { link_token } } = response;
-      // const link_token = 'test1234';
       setToken(link_token);
       console.log(link_token);
     }
@@ -21,26 +19,15 @@ const PlaidComponent: FunctionComponent = () => {
 
   const onSuccess = useCallback<PlaidLinkOnSuccess>(
     (public_token, metadata) => {
-      // send public_token to server
       const res = sendPlaidPublicToken(userID, public_token);
       if (res) {
         console.log(res);
       }
       console.log(metadata);
-      // async function sendPublicToken() {
-      //   const res = await sendPlaidPublicToken(userID, public_token);
-      //   console.log(res);
-      //   console.log(metadata);
-      // }
-      // sendPublicToken();
     },
     [],
   );
 
-  // The pre-built PlaidLink component uses the usePlaidLink hook under the hood.
-  // It renders a styled button element and accepts a `className` and/or `style` prop
-  // to override the default styles. It accepts any Link config option as a prop such
-  // as receivedRedirectUri, onEvent, onExit, onLoad, etc.
   return token === null ? (
     // insert your loading animation here
     <div className='loader' />
@@ -48,6 +35,7 @@ const PlaidComponent: FunctionComponent = () => {
     <PlaidLink
       token={token}
       onSuccess={onSuccess}
+      // className=''
       // onExit={...}
       // onEvent={...}
     >
